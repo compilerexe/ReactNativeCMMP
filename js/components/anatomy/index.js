d582ad5e3a50a6525467e0bad87c7f4257468585
Compiler Exe <compilerexe@gmail.com>
2017-12-14 11:26:39 +0700

    design schedule.

--- js/components/anatomy/index.js
+++ js/components/anatomy/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {
   Container,
   Header,
@@ -12,48 +12,100 @@ import {
   FooterTab,
   Left,
   Right,
-  Body
-} from "native-base";
+  Body,
+  Card,
+  CardItem
+} from 'native-base'
 
-import styles from "./styles";
+import { View } from 'react-native'
+
+import styles from './styles'
 
 class Anatomy extends Component {
-  render() {
+  render () {
     return (
       <Container style={styles.container}>
-        <Header>
+        <Header style={{marginTop: 20, paddingBottom: 15}}>
           <Left>
             <Button
               transparent
-              onPress={() => this.props.navigation.navigate("DrawerOpen")}
+              onPress={() => this.props.navigation.navigate('DrawerOpen')}
             >
-              <Icon name="ios-menu" />
+              <Icon name="ios-menu"/>
             </Button>
           </Left>
           <Body>
-            <Title>Header</Title>
+          <Title style={{width: 250}}>Chiang Mai MAKER PARTY</Title>
           </Body>
-          <Right />
+          <Right/>
 
         </Header>
 
         <Content padder>
-          <Text>
-            Content goes here
-          </Text>
+
+          <Card style={styles.mb}>
+
+            <CardItem header bordered style={{backgroundColor: '#3AB4F1'}}>
+              <Text style={{fontWeight: 'bold', color: 'white'}}>
+                Wednesday 06 / 12 / 2560
+              </Text>
+            </CardItem>
+
+            <CardItem bordered style={{flex: 1, flexDirection: 'column'}}>
+
+              <Text style={{ color: 'green', fontWeight: 'bold'}}>10.00 : 11.00 , Activity</Text>
+              <Text>{`การบัดกรีวงจรอิเล็กโทรนิกส์พื้นฐาน CMMC`}</Text>
+
+              <View style={{justifyContent: 'center', marginTop: 10, marginBottom: 10}}>
+                <Button danger>
+                  <Icon active name="heart" style={{color: 'white', marginRight: 0}}/>
+                  <Text>
+                    Interesting
+                  </Text>
+                </Button>
+              </View>
+
+            </CardItem>
+
+            <CardItem bordered style={{flex: 1, flexDirection: 'column'}}>
+
+              <Text style={{ color: 'green', fontWeight: 'bold'}}>11.00 : 12.00 , Maker Talk</Text>
+              <Text>{`Maker Movement in South East Asia`}</Text>
+
+              <View style={{justifyContent: 'center', marginTop: 10, marginBottom: 10}}>
+                <Button danger>
+                  <Icon name="heart" style={{color: 'white', marginRight: 0}}/>
+                  <Text>
+                    Interesting
+                  </Text>
+                </Button>
+              </View>
+
+            </CardItem>
+
+            <CardItem bordered style={{flex: 1, flexDirection: 'column'}}>
+
+              <Text style={{ color: 'green', fontWeight: 'bold'}}>12.00 : 13.00 , Activity</Text>
+              <Text>{`Espresso Lite WiFi Rover`}</Text>
+
+              <View style={{justifyContent: 'center', marginTop: 10, marginBottom: 10}}>
+                <Button danger>
+                  <Icon name="heart" style={{color: 'white', marginRight: 0}}/>
+                  <Text>
+                    Interesting
+                  </Text>
+                </Button>
+              </View>
+
+            </CardItem>
+
+          </Card>
 
         </Content>
 
-        <Footer>
-          <FooterTab>
-            <Button active full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
       </Container>
-    );
+    )
   }
 }
 
-export default Anatomy;
+export default Anatomy
--- js/components/anatomy/styles.js
+++ js/components/anatomy/styles.js
@@ -1,9 +1,25 @@
-const React = require("react-native");
+const React = require('react-native')
 
-const { StyleSheet } = React;
+const {StyleSheet} = React
 
 export default {
   container: {
-    backgroundColor: "#fff"
+    backgroundColor: '#fff'
+  },
+  text: {
+    alignSelf: 'center',
+    marginBottom: 7
+  },
+  mb: {
+    marginBottom: 15
+  },
+  mb15: {
+    marginBottom: 20
+  },
+  mt15: {
+    marginTop: 15
+  },
+  mb20: {
+    marginBottom: 20
   }
-};
+}
